8563f4069e34771b76223bc3c543bd6686510448
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-11-01 09:54:48 +0100

    Getting started with ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,23 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Hash each ticket so the starting location is the key and the desination is the value.
+    for ticket in tickets:
+        print(ticket.source)
+        print(ticket.destination)
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    # Find the n-th location in the route by checking the hashtable for the n-1th
+    print(hashtable.storage)
+
+    # print(current_ticket)
 
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, 3)
